e78b3460de4668721dcec559f79310d53bd58518
Den4ik1992 <82113641+Den4ik1992@users.noreply.github.com>
2024-10-29 22:15:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
 import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'
-    import { resolve } from 'path'
-    import tailwindcss from 'tailwindcss'
-    import autoprefixer from 'autoprefixer'
-    
-    export default defineConfig({
-      plugins: [react()],
-      css: {
-        postcss: {
-          plugins: [
-            tailwindcss({
-              content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-              theme: {
-                extend: {},
-              },
-              plugins: [],
-            }),
-            autoprefixer,
-          ],
-        },
-      },
-    })
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss({
+          content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+          theme: {
+            extend: {},
+          },
+          plugins: [],
+        }),
+        autoprefixer,
+      ],
+    },
+  },
+  server: {
+    host: '0.0.0.0',  // Erlaubt externen Zugriff
+    port: 4173,       // Port für den Server
+  },
+})
